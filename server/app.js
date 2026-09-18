c20d454206bcef469b7257a409cfb7c16a5003e6
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-09-18 18:51:50 +0000

    ✨ Feat: Carpeta back-end Set up Express application with routing and error handling

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Configurar la carpeta de archivos estaticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Registramos las rutas de la aplicacion
 app.use('/', indexRouter);
