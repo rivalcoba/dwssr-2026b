3a1692dfb4075e169ca145fe1068f8b62e5ca617
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-09-25 18:58:33 +0000

    ✨ Feat: Nodemon Update users route response and enhance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1 style="color:red">LISTA DE AMIGAS</h1>');
 });
 
 // module.exports = router;
